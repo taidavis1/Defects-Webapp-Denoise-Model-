d0fa61365776a79a0e2876d2a3867d76a3d39429
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-11-14 13:08:10 -0800

    setup all packages need it

---
+++ front_end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <main className = "" >
+      <Home />
+    </main>
+  );
+}
+
+export default App;
---
+++ front_end/src/Components/Home.js
@@ -0,0 +1,7 @@
+import React , {useState} from "react";
+
+export default function Home(){
+    return (
+        <div className=" text-lg text-rose-300">Hello world</div>
+    );
+};
