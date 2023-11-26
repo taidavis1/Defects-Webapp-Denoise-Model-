69c104c2f280e0b4a6e2d7b1b6d68f7d9457e517
desktop-fbd3sph <jacobpham82@gmail.com>
2023-11-26 15:14:20 -0800

    changes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
-import Home from "./Components/Home";
-import Input from "./Components/Input.js"
-import Picture from "./Components/Picture.js"
-import WaferData from "./Components/WaferData.js"
-import { Helmet } from 'react-helmet';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowLeft , faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
 
 function App() {
+
+  const [isClose , SetisClose] = useState(true);
+
   return (
     <main className = "" >
-      <Helmet>
-        <title>Wafer Defect Detection</title>
-      </Helmet>
-      <Input className="Input"/>
-      <Picture className="Picture"/>
-      <WaferData className="WaferData"/>
+
+      <div className = "flex">
+        <div className = {`${isClose ? "md:w-1/3": "w-0"} p-5 h-screen transition-all duration-1000 delay-100 ease-in-out bg-blue-300 relative`}>
+          <div onClick={() => SetisClose(!isClose)} className=' absolute shadow-md bg-white -right-3 cursor-pointer border-2 border-sky-400 rounded-full px-2 py-1'>
+            {isClose? 
+              <FontAwesomeIcon className=' text-xl text-sky-500' icon={faArrowLeft} />:
+              <FontAwesomeIcon className=' text-xl text-sky-500' icon={faArrowRight} />
+            }
+          </div>
+        </div>
+        <div className='p-8'>
+            <div className=' border-2 w-[400px]  h-1/2'>
+              dadad
+            </div>
+          </div>
+          <div className='p-8'>
+            <div className=' border-2  w-[400px] h-1/2'>
+              dadad
+            </div>
+          </div>
+      </div>      
     </main>
   );
 }
--- front_end/src/Components/Home.js
+++
@@ -1,7 +0,0 @@
-import React , {useState} from "react";
-
-export default function Home(){
-    return (
-        <div className=" text-lg text-rose-300">Hello world</div>
-    );
-};
